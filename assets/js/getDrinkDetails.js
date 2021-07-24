622222102e4f949255ace9005c4cc393b5a5bfc8
Helena Martins <lenatinsjs@gmail.com>
2021-07-24 19:31:00 -0300

    adding modal to template

--- assets/js/getDrinkDetails.js
+++ assets/js/getDrinkDetails.js
@@ -1,31 +1,3 @@
-const populateMain = (target, template) => {
-  const newTarget = document.querySelector(target);
-  newTarget.innerHTML = ""
-  newTarget.innerHTML = template;
-}
-
-const fetchData = (endpoint) => {
-  const response = fetch(endpoint).then(res => res.json());
-  return response
-}
-
-const templateDrink = (drink) => { 
-  console.log(drink)
-  const {strDrinkThumb, strDrink, idDrink, strAlcoholic, strGlass} = drink;
-    return `<div class="drinksDetails" onclick="showData(${idDrink})">
-    <figure class="smallThumb">
-    <img src="${strDrinkThumb || "https://via.placeholder.com/500x500.png?text=Image+unavailable"}" />
-    </figure>
-    <div class="drinksDetailsHead">
-      <small>ID: ${idDrink}</small>
-      <h3>${strDrink}</h3>
-      <p>Type: ${strAlcoholic}</p>
-      <p>Glass: ${strGlass}</p>
-    </div>
-    </div>
-      `
-}
-
 const templateDrinkData = (drink) => {
   const {strDrink, idDrink, strAlcoholic, strCategory, strDrinkThumb, strGlass, strInstructions} = drink;
   let ingredients = [];
@@ -50,7 +22,6 @@ const templateDrinkData = (drink) => {
 
   templateIngredients += '</div></div>'
 
-  console.log(ingredients, measures);
   
   return `<div class="drinkCard grid">
             <div class="thumb">
@@ -71,19 +42,6 @@ const templateDrinkData = (drink) => {
           </div>`
 }
 
-const searchDrink = async(param) =>{  
-  const response = await fetchData(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${param}`)
-  console.log(response);
-  let template = '<div class="drinksInfoList">'
-
-  for(let i = 0; i < response.drinks.length; i++){
-    template += templateDrink(response.drinks[i])
-
-  }
-  template += "</div>";
-  populateMain('.container', template)
-}
-
 const showData = async(id) => {
   const response = await fetchData(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
   const drinkData = response.drinks[0]
@@ -98,7 +56,6 @@ console.log(drinkData)
 
   
   template += "</div>";
-  populateMain('.container', template)
+  populateMain('.modalTarget', template)
+  modal.style.display = "block";
 }
-
-
---
+++ assets/js/getDrinks.js
@@ -0,0 +1,44 @@
+const populateMain = (target, template) => {
+  const newTarget = document.querySelector(target);
+  newTarget.innerHTML = ""
+  newTarget.innerHTML = template;
+}
+
+const fetchData = (endpoint) => {
+  const response = fetch(endpoint).then(res => res.json());
+  return response
+}
+
+const templateDrink = (drink) => { 
+  console.log(drink)
+  const {strDrinkThumb, strDrink, idDrink, strAlcoholic, strGlass} = drink;
+    return `<div class="drinksDetails" onclick="showData(${idDrink})">
+    <figure class="smallThumb">
+    <img src="${strDrinkThumb || "https://via.placeholder.com/500x500.png?text=Image+unavailable"}" />
+    </figure>
+    <div class="drinksDetailsHead">
+      <small>ID: ${idDrink}</small>
+      <h3>${strDrink}</h3>
+      <p>Type: ${strAlcoholic}</p>
+      <p>Glass: ${strGlass}</p>
+    </div>
+    </div>
+      `
+}
+const searchDrink = async(param) =>{  
+  
+  const response = await fetchData(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${param}`)
+  
+  let template = '<div class="drinksInfoList">'
+
+  for(let i = 0; i < response.drinks.length; i++){
+    template += templateDrink(response.drinks[i])
+
+  }
+  template += "</div>";
+  populateMain('.container', template)
+}
+
+
+
+
